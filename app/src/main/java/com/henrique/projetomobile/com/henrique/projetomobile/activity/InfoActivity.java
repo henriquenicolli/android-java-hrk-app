09ee01afb563cc4da6d8a6d9193ae3dcda30d26d
Henrique Nicolli <henrique.nicolli@gmail.com>
2019-05-02 00:30:47 -0300

    adcionado tela de informações

--- app/src/main/java/com/henrique/projetomobile/com/henrique/projetomobile/activity/InfoActivity.java
+++ app/src/main/java/com/henrique/projetomobile/com/henrique/projetomobile/activity/InfoActivity.java
@@ -20,7 +20,6 @@ public class InfoActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case android.R.id.home:
                 this.finish();
-
         }
 
         return true;
